1317ded371832b048eb287f45c04643f63e79c28
Gildon Kirk <gildonkirk@gmail.com>
2017-06-30 14:22:51 -0400

    heatmap works but need to connect to database

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,15 @@
-var map, infoWindow, pos, marker, latitude, longitude, point, date, time, user;
+var map, heatmap, infoWindow, pos, marker, latitude, longitude, point, date, time, user;
 
       
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 15
+  });
+
+    heatmap = new google.maps.visualization.HeatmapLayer({
+    data: getPoints(),
+    map: map
   });
 
   infoWindow = new google.maps.InfoWindow;
@@ -168,4 +173,46 @@ $(document).on('click', '.submitAddress', function(e){
   });
 });
 
+
+
+
+
+function toggleHeatmap() {
+  heatmap.setMap(heatmap.getMap() ? null : map);
+}
+
+function changeGradient() {
+  var gradient = [
+    'rgba(0, 255, 255, 0)',
+    'rgba(0, 255, 255, 1)',
+    'rgba(0, 191, 255, 1)',
+    'rgba(0, 127, 255, 1)',
+    'rgba(0, 63, 255, 1)',
+    'rgba(0, 0, 255, 1)',
+    'rgba(0, 0, 223, 1)',
+    'rgba(0, 0, 191, 1)',
+    'rgba(0, 0, 159, 1)',
+    'rgba(0, 0, 127, 1)',
+    'rgba(63, 0, 91, 1)',
+    'rgba(127, 0, 63, 1)',
+    'rgba(191, 0, 31, 1)',
+    'rgba(255, 0, 0, 1)'
+  ]
+  heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+}
+
+function changeRadius() {
+  heatmap.set('radius', heatmap.get('radius') ? null : 20);
+}
+
+function changeOpacity() {
+  heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+}
+
+// Heatmap data: 500 Points
+function getPoints() {
+  return [
+    new google.maps.LatLng(38.9502497, -77.01614359999996),
+  ];
+};
 
