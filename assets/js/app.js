8868878cc6ae94c26883c106138ad48123032db1
Sofia Wicker <sofiazw@gmail.com>
2017-07-01 14:28:55 -0400

    update with signin and redirect

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,84 +1,83 @@
-// Global Variables
-// I made any internally declared variables global so that I could reuse them for other functions.  
+var map, heatmap, infoWindow, pos, marker, latitude, longitude, point, date, time, user;
 
-var map, infoWindow, pos, marker, point, date, time, user;
-
-// Initializes map - AKA - starts and displays the map on our web-page.
       
 function initMap() {
-
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 15
+  });
+
+    heatmap = new google.maps.visualization.HeatmapLayer({
+    data: getPoints(),
+    map: map
   });
 
   infoWindow = new google.maps.InfoWindow;
 
 // Geolocates the user, if they allow them to!
 
-if (navigator.geolocation) {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-  navigator.geolocation.getCurrentPosition(function(position) {
-    pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
+      // Allows the marker to be movable and sets the marker to be displayed at the user's position.   
+      marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        draggable:true,
+        title:"Drag me!"
+      })
 
-        // Allows the marker to be movable and sets the marker to be displayed at the user's position.   
+      // Visually displays the marker at the user's location.
+      infoWindow.set(marker);
+      infoWindow.open(map);
+      map.setCenter(pos);
+      
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
 
-    marker = new google.maps.Marker({
-      position: pos,
-      map: map,
-      draggable:true,
-      title:"Drag me!"
-    })
-
-        // Visually displays the marker at the user's location.
-
-    infoWindow.set(marker);
-    infoWindow.open(map);
-    map.setCenter(pos);
-    
-  }, function() {
-    handleLocationError(true, infoWindow, map.getCenter());
-  });
-
-} else {
+  } else {
     // Calls the function that handles a user that doesn't use geolocation.  We will need to modify this to display a way for them to input location information.
-  handleLocationError(false, infoWindow, map.getCenter());
-}
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 
-// Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyCpRAXgZfRwym9rBEfn_RmA2kpfStrIAXo",
-        authDomain: "project-1-88de6.firebaseapp.com",
-        databaseURL: "https://project-1-88de6.firebaseio.com",
-        projectId: "project-1-88de6",
-        storageBucket: "project-1-88de6.appspot.com",
-        messagingSenderId: "408567007214"
-    };
+  // Initialize Firebase
+  var config = {
+      apiKey: "AIzaSyCpRAXgZfRwym9rBEfn_RmA2kpfStrIAXo",
+      authDomain: "project-1-88de6.firebaseapp.com",
+      databaseURL: "https://project-1-88de6.firebaseio.com",
+      projectId: "project-1-88de6",
+      storageBucket: "project-1-88de6.appspot.com",
+      messagingSenderId: "408567007214"
+  };
 
-    firebase.initializeApp(config);
+  firebase.initializeApp(config);
 
-// Create a variable to reference the database.
-    var database = firebase.database();
+  // Create a variable to reference the database.
+  var database = firebase.database();
 
-//clicking on save updates the lat and long on html and table.
+  //clicking on save updates the lat and long on html and table.
+  $(document).on("click", '#save', function(event) {
 
-$("#save").on("click", function(event) {
-          
-  point = marker.getPosition();
-  date = moment().format("MM/DD/YY");
-  time = moment().format("hh:mm:ss");
-  user = "";
+    latitude = marker.getPosition().lat();
+    longitude = marker.getPosition().lng();
+    date = moment().format("MM/DD/YY");
+    time = moment().format("hh:mm:ss");
+    user = "";
 
-  document.getElementById("latitude").value = point.lat();
-  document.getElementById("longitude").value = point.lng();
+    document.getElementById("latitude").value = latitude;
+    document.getElementById("longitude").value = longitude;
 
-  updateTable();
+    updateTable();
 
-  // grabUserLocationDataAndHoldData();
-
+// Need to chang this so it is grabbing data from firebase instead of website
+    tableData.push(new google.maps.LatLng(latitude, longitude));
+    console.log(tableData);
+    // grabUserLocationDataAndHoldData();
     event.preventDefault();
 
     // Grabs User's Da
@@ -86,8 +85,8 @@ $("#save").on("click", function(event) {
     var newLocationData = {
         date: date,
         time: time,
-        lat: point.lat(),
-        lng: point.lng(),
+        lat: latitude,
+        lng: longitude,
         user: user
     };
 
@@ -97,17 +96,15 @@ $("#save").on("click", function(event) {
     // Logs everything to console
     console.log(date);
     console.log(time);
-    console.log(point.lat());
-    console.log(point.long());
+    console.log(latitude);
+    console.log(longitude);
 
     // Alert
     alert("Trash location successfully added!");
-
   })
 }
 
 // Geolocation not accepted my user - function.
-
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(browserHasGeolocation ?
@@ -117,62 +114,106 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 }
 
 // Update table with date, time, location information.
-
 function updateTable() {
   $("#trashTable").append("<tr><td id='date'>" + date + "</td><td id='time'>" + time + "</td><td id='point'>" +
-  point + "</td><td id='user'>" + user + "</td></td>");
-
-  // ES: Updated function so id attribute was appended to each item
+  latitude + "</td><td id='point'>" +
+  longitude + "</td><td id='user'>" + user + "</td></td>");
 }
 
-// function grabUserLocationDataAndHoldData() {
-//
-//     event.preventDefault();
-//
-//     // Grabs User's Data
-//     var getDateForDB = $("#date").val().trim();
-//     var getTimeForDB = $("#time").val().trim();
-//     var getPointForDB = $("#point").val().trim();
-//     var getUserForDB = $("#user").val().trim();
-//
-//     // Creates local "temporary" object for holding User's data
-//     var newLocationData = {
-//         date: getDateForDB,
-//         time: getTimeForDB,
-//         point: getPointForDB,
-//         user: getUserForDB
-//     };
-//
-//     // Uploads employee data to the database
-//     database.ref().push(newLocationData);
-//
-//     // Logs everything to console
-//     console.log(newLocationData.date);
-//     console.log(newLocationData.time);
-//     console.log(newLocationData.point);
-//     console.log(newLocationData.user);
-//
-//     // Alert
-//     alert("Trash location successfully added!");
-// }
 
-// // Firebase watcher + initial loader HINT: .on("value")
-// database.ref().on("value", function(snapshot) {
-//
-//     // Log everything that's coming out of snapshot
-//     console.log(snapshot.val());
-//     console.log(snapshot.val().date);
-//     console.log(snapshot.val().time);
-//     console.log(snapshot.val().point);
-//     console.log(snapshot.val().user);
-//
-//     // Change the HTML to reflect
-//     $("#date").html(snapshot.val().date);
-//     $("#time").html(snapshot.val().time);
-//     $("#point").html(snapshot.val().point);
-//     $("#user").html(snapshot.val().user);
-//
-//     // Handle the errors
-// }, function(errorObject) {
-//     console.log("Errors handled: " + errorObject.code);
-// });
+//GeoLocation not accepted my user - function. 
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+  'Error: The Geolocation service failed.' :
+  'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+  streetAdd();
+}
+
+function streetAdd(){
+  $('#map').after('<p id="instruct">Log Trash Site:</p>');
+  $('#instruct').after('<form id="addEnter" class="form-inline"></form>');
+  $('#addEnter').append('<div id="form-group1" class="form-group"></div>');
+  $('#form-group1').append('<label for="street">Address</label>');
+  $('#form-group1').append('<input type="text" class="form-control" id="address" placeholder="123 Street, City, State Zip">');
+  $('#form-group1').append('<button type="submit" class="submitAddress btn btn-default">Enter</button>')
+}
+
+
+$(document).on('click', '.submitAddress', function(e){
+  e.preventDefault();
+  var address = $('#address').val();
+  var geocoder = new google.maps.Geocoder();
+
+  geocoder.geocode({'address': address}, function (result) {
+    latitude = result[0].geometry.location.lat();
+    longitude = result[0].geometry.location.lng();
+    $('#latitude').val(latitude);
+    $('#longitude').val(longitude);
+    console.log(latitude);
+    console.log(longitude);
+    document.getElementById("longitude").value = longitude;
+
+    date = moment().format("MM/DD/YY");
+    time = moment().format("hh:mm:ss");
+    user = '';
+
+    updateTable();
+
+    var newLocationData = {
+        date: date,
+        time: time,
+        lat: latitude,
+        lng: longitude,
+        user: user
+    };
+    database = firebase.database();
+
+    database.ref().push(newLocationData);
+
+    alert("Trash location successfully added!");
+  });
+});
+
+
+
+
+
+function toggleHeatmap() {
+  heatmap.setMap(heatmap.getMap() ? null : map);
+}
+
+function changeGradient() {
+  var gradient = [
+    'rgba(0, 255, 255, 0)',
+    'rgba(0, 255, 255, 1)',
+    'rgba(0, 191, 255, 1)',
+    'rgba(0, 127, 255, 1)',
+    'rgba(0, 63, 255, 1)',
+    'rgba(0, 0, 255, 1)',
+    'rgba(0, 0, 223, 1)',
+    'rgba(0, 0, 191, 1)',
+    'rgba(0, 0, 159, 1)',
+    'rgba(0, 0, 127, 1)',
+    'rgba(63, 0, 91, 1)',
+    'rgba(127, 0, 63, 1)',
+    'rgba(191, 0, 31, 1)',
+    'rgba(255, 0, 0, 1)'
+  ]
+  heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+}
+
+function changeRadius() {
+  heatmap.set('radius', heatmap.get('radius') ? null : 20);
+}
+
+function changeOpacity() {
+  heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+}
+
+var tableData = [];
+// Heatmap data: 500 Points
+function getPoints() {
+  return tableData;
+};
