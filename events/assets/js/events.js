5de752c2808b587ad0da3ce56034de5c0c56a2bb
Gildon Kirk <gildonkirk@gmail.com>
2017-07-05 13:24:05 -0400

    event form submit works, need to connect to firebase still

--- events/assets/js/events.js
+++ events/assets/js/events.js
@@ -1,9 +1,48 @@
 $(document).ready(function() {
-
-    // page is now ready, initialize the calendar...
-
     $('#calendar').fullCalendar({
-        // put your options and callbacks here
-    })
+	})
+});
 
+var config = {
+	apiKey: "AIzaSyCpRAXgZfRwym9rBEfn_RmA2kpfStrIAXo",
+	authDomain: "project-1-88de6.firebaseapp.com",
+	databaseURL: "https://project-1-88de6.firebaseio.com",
+	projectId: "project-1-88de6",
+	storageBucket: "project-1-88de6.appspot.com",
+	messagingSenderId: "408567007214"
+};
+
+firebase.initializeApp(config);		
+var database = firebase.database();
+
+var eventCount = 0;
+var eventName = '';
+var eventDescrip = '';
+var trashPoint = '';
+var eventDate = '';
+var eventTime = '';
+
+var eventInfo = {
+	count: eventCount,
+	name: eventName,
+	description: eventDescrip,
+	address: trashPoint,
+	date: eventDate,
+	time: eventTime
+}
+
+$(document).on('click', '.submit', function(e){
+	e.preventDefault();
+	eventCount++;
+	eventName = $('#name').val();
+	eventDescrip = $('#description').val();
+	trashPoint = $('#meet').val();
+	eventDate = $('#meetDate').val();
+	eventTime = $('#meetTime').val();
+	console.log(eventName);
+	console.log(eventDescrip);
+	console.log(trashPoint);
+	console.log(eventDate);
+	console.log(eventTime);
+	// database.ref().push
 });
